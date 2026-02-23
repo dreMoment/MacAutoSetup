2e005fe515814678f58a55d42a1b0ee36e8681aa
Andrei Munteanu <andrei@student-net-cx-1223.intern.ethz.ch>
2026-02-23 13:25:29 +0100

    Minor Changes
    Changed aerospace hjkl commands to match german keyboard
    Took out unnecessary aliases and added basic git and tmux aliases

--- dotfiles/zsh/.zsh.d/aliases.sh
+++ dotfiles/zsh/.zsh.d/aliases.sh
@@ -1,4 +1,4 @@
-alias db="cd ~/DB && nvim ~/DB/index.md"
+# alias db="cd ~/DB && nvim ~/DB/index.md"
 
 # Prefer GNU tools over BSD ones
 
@@ -6,12 +6,28 @@ alias db="cd ~/DB && nvim ~/DB/index.md"
 alias sed="gsed"
 alias awk="gawk"
 
+# Git aliases
+alias g="git"
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gl="git log --oneline --graph --all"
+alias gco="git checkout"
+alias gb="git branch"
+
+# Tmux aliases
+alias tl="tmux ls"
+alias tn="tmux new-session -s"
+alias ta="tmux attach-session -t"
+alias tk="tmux kill-session -t"
+alias tm="tmux new-session -A -s"
 
 # TODO aliases
-alias todo='todo.sh'
-alias t='todo.sh'
-alias td='todo.sh done'
-alias ta='todo.sh add'
-alias tl='todo.sh list'
-alias tp='todo.sh pri'
-alias te='nvim ~/DB/todo.txt' # let's me just take a look at the todo file
+# alias todo='todo.sh'
+# alias t='todo.sh'
+# alias td='todo.sh done'
+# alias ta='todo.sh add'
+# alias tl='todo.sh list'
+# alias tp='todo.sh pri'
+# alias te='nvim ~/DB/todo.txt' # let's me just take a look at the todo file
